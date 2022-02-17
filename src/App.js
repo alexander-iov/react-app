bfc28128bcf8fe813189e9e080d60ab7b812902e
Oleksandr Iovsa <oleksandr.iovsa@cengage.com>
2022-02-17 10:38:00 +0200

    add PostList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import './styles/App.css'
-import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
 
+  const [posts, setPosts] = useState(
+      [
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript 2', body: 'Description'},
+        {id: 3, title: 'Javascript 3', body: 'Description'}
+      ]
+  );
+
   return (
       <div className="App">
-        <PostItem post={{id: 1, title: 'Javascript', body: 'Description'}}/>
+        <PostList posts={posts} title={'Список постов'}/>
       </div>
   );
 }
