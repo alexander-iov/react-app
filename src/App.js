184c370ec0bbab6e189be789ab0f860aaccc2d2b
Oleksandr Iovsa <oleksandr.iovsa@cengage.com>
2022-02-17 10:26:34 +0200

    add PostItem component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import './styles/App.css'
+import PostItem from "./components/PostItem";
 
 function App() {
-  return (
-      <div>
 
+  return (
+      <div className="App">
+        <PostItem post={{id: 1, title: 'Javascript', body: 'Description'}}/>
       </div>
   );
 }
